a3335837b39142af3377817f385398ae04782488
Tsofiya-Kasher <Tsofiaka@hilma.tech>
2025-11-30 14:37:32 +0200

    app.use

--- server/app.js
+++ server/app.js
@@ -1,20 +1,30 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var connection = require("./db/connection");
+// var create = require("./scripts/createTables");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var userRouter = require("./routes/user.routs");
+var todoRouter = require("./routes/todo.routs");
+var postRouter = require("./routes/post.routs");
+var signUpRouter = require("./routes/signUp.routs");
+var loginRouter = require("./routes/login.routs");
+var commentRouter = require("./routes/comment.routs");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/user", userRouter);
+app.use("/todo", todoRouter);
+app.use("/post", postRouter);
+app.use("/signUp", signUpRouter);
+app.use("/login", loginRouter);
+app.use("/comment", commentRouter);
 
 module.exports = app;
--- server/scripts/createTables.js
+++ server/scripts/createTables.js
@@ -39,15 +39,7 @@ async function createTables() {
     }
   }
   connection.end();
-  // TODO: Execute each CREATE TABLE query using the connection
-  // Hint: Use connection.promise().query() for async/await or connection.query() for callbacks
-  // Hint: Add error handling for each query execution
-
-  // TODO: Close the connection after all tables are created
-  // Hint: Use connection.end()
 }
-// Export the function
-module.exports = { createTables };
+// module.exports = { createTables };
 
-// Uncomment the line below to run this script directly
-// createTables();s
+// createTables();
