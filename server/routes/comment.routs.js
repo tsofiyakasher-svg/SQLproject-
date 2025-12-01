f438e04384ed9d5ccfc8282bb806c8ccf460d96e
efratfeder-rgb <efratfeder@hilma.tech>
2025-12-01 10:21:15 +0200

    login and signup

--- server/routes/comment.routs.js
+++ server/routes/comment.routs.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const connection = require("../db/connection");
-const commentService = require("../routes/services/comment.service");
+const commentService = require("../routes/services/comment.services");
 
 module.exports = router;
--- server/routes/todo.routs.js
+++ server/routes/todo.routs.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const connection = require("../db/connection");
-const todoService = require("../routes/services/todo.service");
+const todoService = require("../routes/services/todo.services");
 
 module.exports = router;
